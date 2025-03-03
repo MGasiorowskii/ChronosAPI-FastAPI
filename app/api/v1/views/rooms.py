078c21efa565864c0f10e4b19c7567a734bb561f
Mateusz Gasiorowski <gasiorowskii.mateusz@gmail.com>
2025-03-03 17:57:59 +0000

    feat: add retrive and create view for ConferenceRoom

--- app/api/v1/views/rooms.py
+++ app/api/v1/views/rooms.py
@@ -1,17 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
-from models import ConferenceRoom, User
+from models import ConferenceRoom, User, ResponseConferenceRoom, CreateConferenceRoom
 from api.v1.deps import SessionDep, CurrentUser
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=list[ResponseConferenceRoom])
 def read_rooms(session: SessionDep, user: CurrentUser):
     query = session.exec(
         select(ConferenceRoom)
         .join(ConferenceRoom.manager)
         .where(User.company_id == user.company_id)
     ).all()
-    return query
+    return [ResponseConferenceRoom.from_orm(room) for room in query]
+
+
+@router.get("/{room_id}", response_model=ResponseConferenceRoom)
+def read_room(room_id: int, session: SessionDep, user: CurrentUser):
+    query = session.exec(
+        select(ConferenceRoom)
+        .join(ConferenceRoom.manager)
+        .where(User.company_id == user.company_id)
+        .where(ConferenceRoom.id == room_id)
+    ).one_or_none()
+    return ResponseConferenceRoom.from_orm(query) if query else []
+
+
+@router.post("/", response_model=ResponseConferenceRoom)
+def create_room(data: CreateConferenceRoom, session: SessionDep, user: CurrentUser):
+    manager = session.exec(
+        select(User).where(
+            User.email == data.manager, User.company_id == user.company_id
+        )
+    ).one_or_none()
+    if not manager:
+        raise HTTPException(status_code=400, detail="Manager not found")
+
+    room = ConferenceRoom(
+        name=data.name,
+        address=data.address,
+        manager_id=manager.id,
+    )
+    session.add(room)
+    session.commit()
+    session.refresh(room)
+    return ResponseConferenceRoom.from_orm(room)
--- app/models/rooms.py
+++ app/models/rooms.py
@@ -1,15 +1,37 @@
 import uuid
 
+from pydantic import EmailStr
 from sqlmodel import SQLModel, Field, Relationship
 
 
-class ConferenceRoom(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class RoomBase(SQLModel):
     name: str = Field(max_length=255)
     address: str = Field(max_length=255)
 
+
+class ResponseConferenceRoom(RoomBase):
+    id: int
+    manager: EmailStr
+
+    @classmethod
+    def from_orm(cls, room, **kwargs):
+        return cls(
+            id=room.id,
+            name=room.name,
+            address=room.address,
+            manager=room.manager.email if room.manager else None,
+        )
+
+
+class CreateConferenceRoom(RoomBase):
+    manager: EmailStr
+
+
+class ConferenceRoom(RoomBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     manager_id: uuid.UUID | None = Field(
         default=None, foreign_key="user.id", ondelete="SET NULL"
     )
+
     manager: "User" = Relationship(back_populates="managed_rooms")
     events: list["CalendarEvent"] = Relationship(back_populates="location")
